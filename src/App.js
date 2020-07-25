9fefcdb5f772604b98f77743f1f3a3186e9a42c1
Nguyễn Hữu Nghĩa <nguyenhuunghia152@gmail.com>
2020-07-25 23:18:44 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {NavigationNativeContainer} from '@react-navigation/native';
+import {ApolloProvider} from '@apollo/react-hooks';
+
+import AppNavigator from './navigation/AppNavigator';
+import ContextProvider from './tools/context';
+import client from './tools/apollo';
+import './tools/firebase';
+import {YellowBox} from 'react-native';
+
+// https://github.com/facebook/react-native/issues/12981
+YellowBox.ignoreWarnings(['Setting a timer']);
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <ContextProvider>
+        <NavigationNativeContainer>
+          <AppNavigator />
+        </NavigationNativeContainer>
+      </ContextProvider>
+    </ApolloProvider>
+  );
+}
+
+export default App;
